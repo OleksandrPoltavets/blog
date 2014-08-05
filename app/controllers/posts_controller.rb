9e74909f0abda0305ee2247a28a3039144b23538
Oleksandr Poltavets <sancho.ck@gmail.com>
2014-08-06 00:17:43 +0300

    posts-fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :correct_user,   only: :destroy
 
   def create
     @post = current_user.posts.build(post_params)
@@ -12,7 +13,8 @@ class PostsController < ApplicationController
   end
 
   def destroy
-
+    @post.destroy
+    redirect_to root_url
   end
   
   private
@@ -21,4 +23,8 @@ class PostsController < ApplicationController
       params.require(:post).permit(:content)
     end
 
+  def correct_user
+    @post = current_user.posts.find_by(id: params[:id])
+    redirect_to root_url if @post.nil?
+  end
 end
